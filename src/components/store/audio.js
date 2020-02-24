bf8a448ff0282ff2bcea1d8de1f29bb339e92faf
Denis Davydenko <iamstylle@gmail.com>
2020-02-24 13:49:49 +0300

    Added bing.webm

--- src/components/store/audio.js
+++ src/components/store/audio.js
@@ -1,6 +1,6 @@
 import { Howl } from 'howler';
 
-const sound = new Howl({ src: ['bing.mp3'] });
+const sound = new Howl({ src: ['bing.webm', 'bing.mp3'] });
 export function play() {
   sound.stop();
   sound.play();
