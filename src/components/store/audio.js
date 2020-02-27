d8fe20bd4bb1996e3d343a60e22cad19b48e3c14
Denis Davydenko <iamstylle@gmail.com>
2020-02-27 17:04:11 +0300

    Retry adio play on error

--- src/components/store/audio.js
+++ src/components/store/audio.js
@@ -1,6 +1,14 @@
 import { Howl } from 'howler';
 
-const sound = new Howl({ src: ['bing.webm', 'bing.mp3'] });
+const sound = new Howl({
+  src: ['bing.webm', 'bing.mp3'],
+  onplayerror: () => {
+    sound.once('unlock', () => {
+      sound.play();
+    });
+  }
+});
+
 export function play() {
   sound.stop();
   sound.play();
