a351829036478af247a81fe233e396888a21d989
Denis Davydenko <iamstylle@gmail.com>
2020-02-27 17:01:35 +0300

    Changed ratio

--- src/components/consts.js
+++ src/components/consts.js
@@ -2,7 +2,7 @@ export const DEFAULT_SETTINGS = { coffee: 18, water: 305 };
 
 export const SETTINGS_KEY = 'settings';
 
-export const RATIO = 1 / 17;
+export const RATIO = 1 / 16.666667;
 
 export const AMOUNT_PLACEHOLDER = '{amount}';
 
